5e00a60d6e9a4ee042d6f064adc5a2279ac71ae9
amine759 <saihiamine569@gmail.com>
2022-08-22 01:04:05 +0000

    .

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('model.yml')
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+labels=['amine','Ghassane','giannis']
+
+cap=cv2.VideoCapture(0)
+
+while True:
+    ref,frame = cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(gray)
+
+    for (x,y,w,h) in faces:
+
+        box=gray[y:y+h,x:x+w]
+        label,accuracy=face_recognizer.predict(box)
+        prediction = labels[label-1]
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
+        cv2.putText(frame, prediction, (x, y), cv2.FONT_HERSHEY_PLAIN, 6, (0, 255, 255), 5)
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import numpy as np 
 import cv2
-import os 
+import os   
 
-labels={'amine':1,'Ghassane':2,'giannis':3}
+labels=['amine','Ghassane','giannis']
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 def detect_face(img):
@@ -22,50 +22,58 @@ def draw_rectangle(img, rect):
 def prepare_data(folder):
     x_train=[]
     y_labels=[]
+    index=0
     dirs = os.listdir(folder)
     for dir_name in dirs :
+        index+=1
         abs_path_dir = folder + "/" + dir_name
         names = os.listdir(abs_path_dir)
         for img_name in names :
             img_path = abs_path_dir + "/" + img_name
             img = cv2.imread(img_path)
-            cv2.imshow("Training on image...", cv2.resize(img, (600, 500)))
             cv2.waitKey(100)
             face, rect = detect_face(img)
             draw_rectangle(img,rect)
+            cv2.imshow("Training on images", cv2.resize(img, (600, 400)))
 
             if face is not None :
-               x_train.append(face)
-               y_labels.append(labels[dir_name])
+                x_train.append(face)
+                y_labels.append(index)
+                         
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
+
     return x_train, y_labels
 
 faces,y_labels = prepare_data("dataset")
 print("Total faces:", len(faces))
 
-face_recognizer.train(faces, np.array(y_labels))
-#recognizor.save('model.yml')
-
-
+#face_recognizer.train(faces, np.array(y_labels))
+#face_recognizer.save('model.yml')
+face_recognizer.read('model.yml')
 
 def draw_text(img, text, x, y):
-    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
-
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 6, (0, 255, 0), 5)
 
 def predict(test_img):
     img = test_img.copy()
     face, rect = detect_face(img)
 
     label, confidence = face_recognizer.predict(face)
-    for key,value in labels.items() :  
-        if value == label :
-            label_text = key
-
+    prediction = labels[label-1]
     draw_rectangle(img, rect)
-    draw_text(img, label_text, rect[0], rect[1]-5)
+    draw_text(img, prediction, rect[0], rect[1]-5)
     return img
 
-test_path ="dataset/amine/" + os.listdir("dataset/amine")[0]
-
+test_path ="dataset/Ghassane/" + os.listdir("dataset/Ghassane")[0]
+print(test_path)
 test_img = cv2.imread(test_path)
 prediction1 = predict(test_img)
-cv2.imshow("test", cv2.resize(prediction1, (600, 500)))
+cv2.imshow("test", cv2.resize(prediction1, (600, 400)))
+cv2.waitKey(0)
+
+if 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)  
+    cv2.destroyAllWindows()
